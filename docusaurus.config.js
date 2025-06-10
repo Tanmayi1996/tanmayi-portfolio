8095858dd539835bffb7f5c35363ef9bed9cc08c
tanmayi.s <tanmayi.s@BMIDTANMAYIRS.local>
2025-06-10 16:22:55 +0530

    initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Tanmayi1996.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tanmayi-portfolio/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Tanmayi1996', // Usually your GitHub org/user name.
+  projectName: 'tanmayi-portfolio', // Usually your repo name.
+  deploymentBranch: 'gh-pages',    // Default by Docusaurus
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
